78ae86530048822850b2c9cd6c0129bdb9611831
Mr. Sanjoy <--add>
2024-01-16 21:14:23 +0530

    type module is init

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
 
+// const express = require('express')
+import express from 'express'
+const app = express()
+const PORT = process.env.PORT || 3000;
 
-const express = require('express');
-const app = express();
-const port = 1000;
+const data_product = [
+    {
+        id: 1,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        new_price: 50.00,
+        old_price: 80.50,
+    },
+    {
+        id: 2,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        new_price: 85.00,
+        old_price: 120.50,
+    },
+    {
+        id: 3,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        new_price: 60.00,
+        old_price: 100.50,
+    },
+    {
+        id: 4,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        new_price: 100.00,
+        old_price: 150.00,
+    },
+];
+
+
+const person = {
+    name: 'Sanjoy Gorai',
+    age: 22,
+    mobile: 9064,
+    country: 'India'
+}
 
 app.get('/', (req, res) => {
-    res.send("<h1>This is App/Folder Page dsd  </h1> <button>Button</button>");
-});
+    console.log(req.url);
+    res.send("Hello from Express JS.");
+})
 
-app.get('/route', (req, res) => {
-    res.send(" <h1>Toute Page </h1>");
-});
-
-app.get('/about/', (req, res) => {
-    res.send("<button>Button This is about page</button>");
+app.get('/person', (req, res) => {
+    res.send(person)
+    console.log(req.url);
 });
 
 app.get('/products', (req, res) => {
-    res.send('<img src="assets/images/oneplus.webp" alt="phone">');
+    res.send(data_product);
+    console.log(data_product);
 });
 
-app.listen(port, 'localhost', () => {
-    console.log(`Server has been running at ${port} port`);
-});
+
+app.listen(PORT, () => {
+    console.log("Server is running on: " + PORT);
+})
+
+
